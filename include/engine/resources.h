7d97f75eb2788f4512254064a4641db87b2df388
sean-brock <4583759+sean-brock@users.noreply.github.com>
2023-04-07 13:37:54 -0500

    removed empty files

--- include/engine/resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENGINE_RESOURCES_H
-#define ENGINE_RESOURCES_H
-
-#include <stdexcept>
-
-namespace engine {};  // namespace engine
-#endif
--- src/resources.cpp
+++
@@ -1,3 +0,0 @@
-#include <engine/resources.h>
-
-using namespace engine;
